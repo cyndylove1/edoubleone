8fa46fb4eccfe71a96251652874da69cec47d7b2
Nweke Cynthia Chinenye <nwekecynthia2015@gmail.com>
2026-08-27 01:52:06 -0700

    completed

--- src/components/homeCta.tsx
+++ src/components/homeCta.tsx
@@ -15,7 +15,7 @@ export const HomeCta: React.FC<CtaSectionProps> = ({
   
 }) => {
   return (
-    <section className="w-full min-h-[450px] bg-(--primary) flex flex-col items-center justify-center px-6 pt-32 pb-24 text-center ">
+    <section className="w-full min-h-[400px] bg-(--primary) flex flex-col items-center justify-center px-6 pt-32 pb-24 text-center ">
       {/* Title Header */}
       <h2 className="text-white text-5xl md:text-8xl lg:text-[120px] font-[700] tracking-tight lg:leading-[120px] uppercase select-none mb-10">
         <div>{titleLine1}</div>
--- src/components/readyCta.tsx
+++ src/components/readyCta.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export const ReadyCta: React.FC = () => {
   return (
-    <section className="text-white h-[550px] flex flex-col justify-between px-6 md:py-24 py-20 border-b border-t border-[#26252B]">
+    <section className="text-white min-h-[400px] flex flex-col justify-between px-6 md:py-24 py-20 border-b border-t border-[#26252B]">
       {/* Main CTA Content */}
       <div className="flex-1 flex flex-col items-center justify-center text-center max-w-5xl mx-auto">
         <h2 className=" text-5xl md:text-8xl lg:text-[96px] font-[500] tracking-[-2.56px]  leading-[1.05] uppercase lg:leading-[96px] text-[#F0F0F0] mb-10">
