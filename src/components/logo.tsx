f6cc31ffe63831101aa141003441f64e52389a48
Nweke Cynthia Chinenye <nwekecynthia2015@gmail.com>
2026-08-28 11:58:24 -0700

    completed

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,10 @@
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 
 export default function Logo() {
-    return (
-      <Link to="/">
-        {/* Brand Logo */}
-        <div className="w-full h-full">
-          <img src={logo} alt="logo" className="h-[35px] w-full"/>
-        </div>
-      </Link>
-    );
+  return (
+    <Link to="/" className="inline-block">
+      <img src={logo} alt="logo" className="h-[35px] w-auto object-contain" />
+    </Link>
+  );
 }
